494632bff5e6a8d9d69603a07f25d6eeaeab2c91
gilbertoJustino <giljssjustino@gmail.com>
2025-12-07 09:29:43 -0300

    Configuração inicial do projeto com Spring Boot e PostgreSQL

---
+++ src/main/java/com/jigmaster/JigMasterApplication.java
@@ -0,0 +1,13 @@
+package com.jigmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JigMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JigMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jigmaster/JigMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jigmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JigMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
